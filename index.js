e0c8725bb8961ae11365755fdf2dc2fa4fafa708
vivmaniaa <vivmaniaa@gmail.com>
2020-05-20 20:14:16 +0000

    reduce functionality added

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-var fs  = require('fs');
-
-var output = fs.readFileSync('data.txt', 'utf-8')
-.split('\n')
-.map(line=>line.split('\t'))
-.reduce((newAr, b)=>{
-newAr[b[0]] = newAr[b[0]] || [];
-newAr[b[0]].push({
-    name: b[1],
-    price: b[2],
-    quantity: b[3]
-  })
-return newAr;
-}, {});
-
-
-console.log(output);
-
+require('./reduce.js');
